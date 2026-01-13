f5a675a9b02e7e64d8b68892c3a8565ba04a8f3a
Viniciusm15 <viniciushiagomartins00@gmail.com>
2026-01-13 15:56:46 -0300

    feat: criação do servidor express.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Estrutura inicial do projeto.")
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Definir rotas
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`O servidor está executando na porta ${port}`);
+});
